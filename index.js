0c639cc158f024bd89fe8d54eeb02fb179c3959c
Your Name <you@example.com>
2024-04-01 00:46:10 +0500

    I have added 2 new operators called Modulus and Exponentiation

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
+console.log("Welcome to my Calculator");
+101;
 const answer = await inquirer.prompt([
     { message: "Enter Your First Number", type: "number", name: "firstNumber" },
     { message: "Enter Your Second Number", type: "number", name: "secondNumber" },
@@ -7,7 +9,7 @@ const answer = await inquirer.prompt([
         message: "Select one of the Operators to perform Operation",
         type: "list",
         name: "operator",
-        choices: ["Addition", "Subtraction", "Multiplication", "Division"]
+        choices: ["Addition", "Subtraction", "Multiplication", "Division", "Modulus", "Exponentiation"]
     },
 ]);
 if (answer.operator === "Addition") {
@@ -21,6 +23,12 @@ else if (answer.operator === "Multiplication") {
 }
 else if (answer.operator === "Division") {
     console.log(answer.firstNumber / answer.secondNumber);
+}
+else if (answer.operator === "Modulus") {
+    console.log(answer.firstNumber % answer.secondNumber);
+}
+else if (answer.operator === "Exponentiation") {
+    console.log(answer.firstNumber ** answer.secondNumber);
 }
 else {
     console.log("Please Select Valid Operator");
--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@
 
 import inquirer from "inquirer";
 
+console.log ("Welcome to my Calculator")
+101
 const answer = await inquirer.prompt([
   { message: "Enter Your First Number", type: "number", name: "firstNumber" },
   { message: "Enter Your Second Number", type: "number", name: "secondNumber" },
@@ -9,7 +11,7 @@ const answer = await inquirer.prompt([
     message: "Select one of the Operators to perform Operation",
     type: "list",
     name: "operator",
-    choices: ["Addition","Subtraction","Multiplication","Division"]
+    choices: ["Addition","Subtraction","Multiplication","Division","Modulus","Exponentiation"]
   },
 ]);
 
@@ -21,8 +23,13 @@ if (answer.operator === "Addition") {
     console.log (answer.firstNumber * answer.secondNumber)
 } else if (answer.operator === "Division") {
     console.log (answer.firstNumber / answer.secondNumber)
+} else if (answer.operator === "Modulus") {
+    console.log (answer.firstNumber % answer.secondNumber)
+} else if (answer.operator === "Exponentiation") {
+    console.log (answer.firstNumber ** answer.secondNumber)
 } else {
     console.log ("Please Select Valid Operator")
 }
+
 
 console.log ("The End")
